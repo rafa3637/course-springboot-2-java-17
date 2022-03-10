694932210824bc8994504bbdd5d179687542429d
rafa3637 <rafaoliverpi@gmail.com>
2022-03-09 21:42:20 -0300

    Category

---
+++ src/main/java/com/rafasproject/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.rafasproject.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rafasproject.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
